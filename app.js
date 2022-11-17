cc7787dd7eae0665cbdff16841fd68fc78efa2d0
Travis Alstrand <travisalstrand@gmail.com>
2022-11-16 16:14:59 -0800

    completed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const qwerty = document.getElementById('qwerty');
+const buttons = document.querySelectorAll('button');
 const phrase = document.getElementById('phrase');
 const phraseUl = phrase.firstElementChild;
 const overlay = document.getElementById('overlay');
+const hearts = document.querySelectorAll('.tries img');
 const startButton = document.querySelector('.btn__reset');
 
 let missed = 0;
@@ -14,9 +16,25 @@ const phrases = [
 ];
 
 startButton.addEventListener('click', () => {
-  overlay.style.display = 'none';
+  startNewGame();
+});
+
+const startNewGame = () => {
+  const phraseLis = phraseUl.children;
+  if (phraseLis.length) {
+    phraseUl.innerHTML = '';
+  }
+  missed = 0;
   getRandomPhraseAsArray(phrases);
-})
+  buttons.forEach(button => {
+    button.disabled = false;
+    button.className = '';
+  })
+  hearts.forEach(heart => {
+    heart.src = './images/liveHeart.png';
+  })
+  overlay.style.display = 'none';
+};
 
 const getRandomPhraseAsArray = (arr) => {
   const randomNumber = Math.floor(Math.random() * arr.length);
@@ -39,5 +57,49 @@ const addPhraseToDisplay = (arr) => {
 }
 
 const checkLetter = (button) => {
-  
+  const btnLetter = button.innerHTML;
+  const phraseLetters = document.querySelectorAll('.letter');
+  let match = null;
+  phraseLetters.forEach(letter => {
+    if (letter.innerHTML === btnLetter) { 
+      letter.classList.add('show');
+      match = letter.innerHTML;
+    }
+  })
+  return match;
 };
+
+qwerty.addEventListener('click', e => {
+  if (e.target.tagName === 'BUTTON') {
+    const button = e.target;
+    button.classList.add('chosen');
+    button.disabled = true;
+
+    const letterFound = checkLetter(button);
+
+    if (letterFound === null) {
+      hearts[missed].src = './images/lostHeart.png';
+      missed++;
+    }
+
+    checkWin();
+  }
+})
+
+const checkWin = () => {
+  const shownLetters = document.querySelectorAll('.show');
+  const phraseLetters = document.querySelectorAll('.letter');
+  if (shownLetters.length === phraseLetters.length && missed < 5) {
+    overlay.className = '';
+    overlay.classList.add('win');
+    overlay.firstElementChild.innerHTML = 'You Won!';
+    overlay.lastElementChild.innerHTML = 'Play Again?';
+    overlay.style.display = 'inherit';
+  } else if (missed >= 5) {
+    overlay.className = '';
+    overlay.classList.add('lose');
+    overlay.firstElementChild.innerHTML = 'You Lost!';
+    overlay.lastElementChild.innerHTML = 'Play Again?';
+    overlay.style.display = 'inherit';
+  }
+}
